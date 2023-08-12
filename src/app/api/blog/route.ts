d2d4a8ac687865382656aefddf5d1b708d52b38c
devjiwonchoi <devjiwonchoi@gmail.com>
2023-08-13 08:06:59 +0900

    disable blog page before uploading route.ts

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -2,27 +2,28 @@ import { NextResponse, type NextRequest } from "next/server"
 import { getPostsInfo, notionClient } from "@/lib"
 
 export async function GET(request: NextRequest) {
-  const searchParams = await request.nextUrl.searchParams
-  const nextCursor = await searchParams.get('nextCursor') ?? undefined
+  const searchParams = request.nextUrl.searchParams
+  const nextCursor = searchParams.get('nextCursor') ?? undefined
 
   const response = await notionClient.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID as string, filter: {
+    database_id: process.env.NOTION_DATABASE_ID as string,
+    filter: {
       property: 'Status',
       select: {
-        equals: 'ready'
-      }
+        equals: 'ready',
+      },
     },
     sorts: [
       {
         property: 'Date',
-        direction: 'descending'
-      }
+        direction: 'descending',
+      },
     ],
     page_size: 7,
     start_cursor: nextCursor,
     // page_size: 5 * parseInt(pageIndex)
   })
-  const { results } = await response
-  const posts = await getPostsInfo(results)
-  return await NextResponse.json(posts)
+  const { results } = response
+  const posts = getPostsInfo(results)
+  return NextResponse.json(posts)
 }
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,32 +1,36 @@
-import { refinePosts } from '@/lib/notion'
-import NotionPage from '@/notion-jsx/NotionPage'
+// import { refinePosts } from '@/lib/notion'
+// import NotionPage from '@/notion-jsx/NotionPage'
 
-async function getPost(slug: string) {
-  const response = await fetch(`${process.env.API_URL}/blog/${slug}`)
-  const data = await response.json()
-  return data
-}
+// async function getPost(slug: string) {
+//   const response = await fetch(`${process.env.API_URL}/blog/${slug}`)
+//   const data = await response.json()
+//   return data
+// }
 
-export default async function BlogPost({
-  params,
-}: {
-  params: { slug: string }
-}) {
-  const post = await getPost(params.slug)
+// export default async function BlogPost({
+//   params,
+// }: {
+//   params: { slug: string }
+// }) {
+//   const post = await getPost(params.slug)
 
-  return (
-    <main className="p-6 mb-auto">
-      <NotionPage pageData={post} />
-    </main>
-  )
-}
+//   return (
+//     <main className="p-6 mb-auto">
+//       <NotionPage pageData={post} />
+//     </main>
+//   )
+// }
 
-export async function generateStaticParams() {
-  const response = await fetch(`${process.env.API_URL}/blog`)
-  const data = await response.json()
-  const posts = refinePosts(data)
+// export async function generateStaticParams() {
+//   const response = await fetch(`${process.env.API_URL}/blog`)
+//   const data = await response.json()
+//   const posts = refinePosts(data)
 
-  return posts.map((post: any) => ({
-    slug: post.slug,
-  }))
+//   return posts.map((post: any) => ({
+//     slug: post.slug,
+//   }))
+// }
+
+export default function BlogPost() {
+  return <div>BlogPost</div>
 }
