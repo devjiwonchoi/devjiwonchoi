cde4efa74b0f17ba4f0e72f3893a438044004f90
Jiwon Choi <devjiwonchoi@gmail.com>
2023-09-22 23:19:32 +0900

    Bring back Contribs with updated list (#13)

--- src/lib/contribs.ts
+++ src/lib/contribs.ts
@@ -1,3 +1,4 @@
+// TODO: Modify to call GitHub API
 export const contribs = [
   {
     projectName: 'Next.js',
@@ -11,19 +12,37 @@ export const contribs = [
         pullNum: '51104',
         pullHref: 'https://github.com/vercel/next.js/pull/51104',
       },
+      // {
+      //   pullNum: '51148',
+      //   pullHref: 'https://github.com/vercel/next.js/pull/51148',
+      // },
       {
-        pullNum: '51148',
-        pullHref: 'https://github.com/vercel/next.js/pull/51148',
+        pullNum: '54112',
+        pullHref: 'https://github.com/vercel/next.js/pull/54112',
       },
     ],
   },
   {
-    projectName: 'Hyper',
-    projectHref: 'https://github.com/vercel/hyper',
+    projectName: 'Vercel/Edge-Runtime',
+    projectHref: 'https://github.com/vercel/edge-runtime',
     pulls: [
       {
-        pullNum: '7300',
-        pullHref: 'https://github.com/vercel/hyper/pull/7300',
+        pullNum: '533',
+        pullHref: 'https://github.com/vercel/edge-runtime/pull/533',
+      },
+    ],
+  },
+  {
+    projectName: 'Vercel/AI',
+    projectHref: 'https://github.com/vercel/ai',
+    pulls: [
+      {
+        pullNum: '500',
+        pullHref: 'https://github.com/vercel/ai/pull/500',
+      },
+      {
+        pullNum: '510',
+        pullHref: 'https://github.com/vercel/ai/pull/510',
       },
     ],
   },
@@ -38,19 +57,13 @@ export const contribs = [
     ],
   },
   {
-    projectName: 'VSCode',
-    projectHref: 'https://github.com/microsoft/vscode',
-    pulls: [
-      {
-        pullNum: '183955',
-        pullHref: 'https://github.com/microsoft/vscode/pull/183955',
-      },
-    ],
-  },
-  {
     projectName: 'Bunchee',
     projectHref: 'https://github.com/huozhi/bunchee',
     pulls: [
+      {
+        pullNum: '228',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/228',
+      },
       {
         pullNum: '229',
         pullHref: 'https://github.com/huozhi/bunchee/pull/229',
@@ -63,15 +76,103 @@ export const contribs = [
         pullNum: '232',
         pullHref: 'https://github.com/huozhi/bunchee/pull/232',
       },
+      {
+        pullNum: '244',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/244',
+      },
+      {
+        pullNum: '245',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/245',
+      },
+      {
+        pullNum: '249',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/249',
+      },
+      {
+        pullNum: '250',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/250',
+      },
+      {
+        pullNum: '251',
+        pullHref: 'https://github.com/huozhi/bunchee/pull/251',
+      },
     ],
   },
   {
-    projectName: 'AdminJS-SQL',
-    projectHref: 'https://github.com/SoftwareBrothers/adminjs-sql',
+    projectName: 'Vite.js - Docs Korean',
+    projectHref: 'https://github.com/vitejs/docs-ko',
     pulls: [
       {
-        pullNum: '6',
-        pullHref: 'https://github.com/microsoft/vscode/pull/6',
+        pullNum: '644',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/644',
+      },
+      {
+        pullNum: '645',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/645',
+      },
+      {
+        pullNum: '646',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/646',
+      },
+      {
+        pullNum: '647',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/647',
+      },
+      {
+        pullNum: '648',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/648',
+      },
+      {
+        pullNum: '649',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/649',
+      },
+      {
+        pullNum: '651',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/651',
+      },
+      {
+        pullNum: '652',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/652',
+      },
+      {
+        pullNum: '653',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/653',
+      },
+      {
+        pullNum: '654',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/654',
+      },
+      {
+        pullNum: '655',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/655',
+      },
+      {
+        pullNum: '656',
+        pullHref: 'https://github.com/vitejs/docs-ko/pull/656',
+      },
+    ],
+  },
+  {
+    projectName: 'Lichess-API',
+    projectHref: 'https://github.com/lichess-org/api',
+    pulls: [
+      {
+        pullNum: '281',
+        pullHref: 'https://github.com/lichess-org/api/pull/281',
+      },
+      {
+        pullNum: '283',
+        pullHref: 'https://github.com/lichess-org/api/pull/283',
+      },
+    ],
+  },
+  {
+    projectName: 'Berserk',
+    projectHref: 'https://github.com/lichess-org/berserk',
+    pulls: [
+      {
+        pullNum: '46',
+        pullHref: 'https://github.com/lichess-org/berserk/pull/46',
       },
     ],
   },
