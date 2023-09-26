0409b9b08df2742ec6868c81c96a6ac5c9df9add
Jiwon Choi <devjiwonchoi@gmail.com>
2023-09-27 04:40:02 +0900

    feat: add client side console (#14)

---
+++ src/utils/browser-console.tsx
@@ -0,0 +1,9 @@
+'use client'
+export const Console = () => {
+  const str =
+    'Hi Dev! 👋🏻\n' +
+    'Check out this website source on GitHub:\n' +
+    'https://github.com/devjiwonchoi/devjiwonchoi.codes'
+  console.log(str)
+  return null
+}
