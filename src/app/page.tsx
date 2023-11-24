e198814660edc339415f501efb567ff9b97fa837
devjiwonchoi <devjiwonchoi@gmail.com>
2023-11-24 19:54:22 +0900

    update bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,42 +14,69 @@ export default function Bio() {
         <h3 className="mb-4 text-xl font-bold tracking-tight text-neutral-200 sm:text-2xl">
           DevLife
         </h3>
-        <p className="text-base text-neutral-300 sm:text-base">
-          I enjoy to{' '}
+        <p className="text-base text-neutral-200 sm:text-base">
+          I am a software developer who loves to{' '}
           <strong className="text-neutral-50">
-            automate, document, and minimalize
-          </strong>{' '}
-          the development process, focusing on the essentials.
-        </p>
-        <p className="text-base text-neutral-300 sm:text-base">
-          I write my code to be{' '}
-          <strong className="text-neutral-50">
-            self-explanatory and precise
+            automate, simplify, and troubleshoot.
           </strong>
-          , minimizing the need for extensive comments.
+        </p>
+        <p className="text-base text-neutral-200 sm:text-base">
+          My current concentration is contributing to open-source projects such
+          as{' '}
+          <Link
+            href="https://github.com/vercel/next.js"
+            target="_blank"
+            className="underline hover:text-neutral-50"
+          >
+            Next.js
+          </Link>
+          ,{' '}
+          <Link
+            href="https://github.com/microsoft/TypeScript"
+            target="_blank"
+            className="underline hover:text-neutral-50"
+          >
+            TypeScript
+          </Link>
+          , and{' '}
+          <Link
+            href="https://github.com/huozhi/bunchee"
+            target="_blank"
+            className="underline hover:text-neutral-50"
+          >
+            Bunchee
+          </Link>
+          .
         </p>
       </article>
       <article className="mb-6">
         <h3 className="mb-4 text-xl font-bold tracking-tight text-neutral-200 sm:text-2xl">
-          Goal & Interests
+          Ambition
         </h3>
-        <p className="text-base text-neutral-300 sm:text-base">
+        <p className="text-base text-neutral-200 sm:text-base">
           I believe that{' '}
           <strong className="text-neutral-50">
-            saving time and effort for developers
+            saving time for developers and end users
           </strong>{' '}
-          will eventually make the world a bit better place to live.
+          is the key to the success of the product.
         </p>
-        <p className="text-base text-neutral-300 sm:text-base">
-          This is why I chose DevOps, and for the same reason I am interested in
-          compilers.
+        <p className="text-base text-neutral-200 sm:text-base">
+          In the near future, I will become a software engineer who feels
+          confident in writing time-saving codes.
+        </p>
+        <p className="text-base text-neutral-200 sm:text-base">
+          I will be one of the leading engineers who can{' '}
+          <strong className="text-neutral-50">
+            inspire people and positively influence
+          </strong>{' '}
+          others.
         </p>
       </article>
       <article className="mb-6">
         <h3 className="mb-4 text-xl font-bold tracking-tight text-neutral-200 sm:text-2xl">
           Dependencies
         </h3>
-        <p className="text-base text-neutral-300 sm:text-base">
+        <p className="text-base text-neutral-200 sm:text-base">
           Apart from coding, I enjoy playing{' '}
           <Link
             href="https://en.wikipedia.org/wiki/Brazilian_jiu-jitsu"
@@ -73,7 +100,7 @@ export default function Bio() {
         <h3 className="mb-4 text-xl font-bold tracking-tight text-neutral-200 sm:text-2xl">
           Certifications & Awards
         </h3>
-        <ul className="list-inside list-disc text-neutral-300">
+        <ul className="list-inside list-disc text-neutral-200">
           <li>
             <Link
               href="https://www.credly.com/badges/acf8b0bc-2952-4ee1-ac3b-7ab91478ddbb"
