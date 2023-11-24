097ed6d65bb8368c8936ce155626cef0aa1f29a1
devjiwonchoi <devjiwonchoi@gmail.com>
2023-11-25 06:54:25 +0900

    fix: pass lang to html lang

--- src/dictionaries/i18n.ts
+++ src/dictionaries/i18n.ts
@@ -4,8 +4,8 @@ const dictionaries: {
   [key: string]: () => Promise<typeof import('./en.json')>
 } = {
   en: () => import('./en.json').then((module) => module.default),
-  // ko: () => import('./ko.json').then((module) => module.default),
-  // zh: () => import('./zh.json').then((module) => module.default),
+  ko: () => import('./en.json').then((module) => module.default),
+  zh: () => import('./en.json').then((module) => module.default),
 }
 
 export const getDictionary = async (lang: string) => dictionaries[lang]()
