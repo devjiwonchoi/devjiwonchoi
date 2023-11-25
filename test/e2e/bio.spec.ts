65d6277b07d89d082e0e87ed2405faf753059ebe
devjiwonchoi <devjiwonchoi@gmail.com>
2023-11-25 20:42:36 +0900

    test: add bio, blog

--- test/e2e/bio.spec.ts
+++ test/e2e/bio.spec.ts
@@ -1,6 +1,17 @@
 import { test, expect } from '@playwright/test'
 
-test('bio', async ({ page }) => {
-  await page.goto('/')
-  await expect(page).toHaveTitle('Jiwon Choi')
+test.describe('i18n en-US', () => {
+  test.use({ locale: 'en-US' })
+
+  test('should display English contents', async ({ page }) => {
+    await page.goto('/')
+    expect(await page.title()).toBe('Jiwon Choi')
+    expect(await page.textContent('h2')).toBe('Biography')
+  })
+
+  test('should redirect to "/" if "/en"', async ({ page }) => {
+    await page.goto('/en')
+    const pathname = new URL(page.url()).pathname
+    expect(pathname).toBe('/')
+  })
 })
---
+++ test/test-utils.ts
@@ -0,0 +1,1 @@
+export const urlToPathname = (url: string) => new URL(url).pathname
