9bcc7e42b2681125ca6c7fbb398bd4ccafd720b8
devjiwonchoi <devjiwonchoi@gmail.com>
2023-11-25 16:12:23 +0900

    Refactor language handling in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,26 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { i18n } from './dictionaries/i18n'
 
 export function middleware({ url, nextUrl: { pathname } }: NextRequest) {
+  const [curURL, curPathname] = [url, pathname]
   const { langs, defaultLang } = i18n
-  const hasLang = langs.some((lang) => pathname.startsWith(`/${lang}`))
+  const hasLang = langs.some((lang) => curPathname.startsWith(`/${lang}`))
   if (hasLang) return NextResponse.next()
 
-  if (pathname.startsWith(`/${defaultLang}`)) {
-    return NextResponse.redirect(
-      new URL(pathname.replace(new RegExp(`^\\/${defaultLang}`), '/'), url),
+  // TODO: refactor this
+  // pathname: '/en' -> '/'; '/en/a' -> '/a'
+  if (curPathname.startsWith(`/${defaultLang}`)) {
+    const pathnameWithoutDefaultLangRegex = new RegExp(`^/${defaultLang}(.*)`)
+    const pathnameWithoutDefaultLang = curPathname.replace(
+      pathnameWithoutDefaultLangRegex,
+      '$1',
     )
+    return NextResponse.redirect(new URL(pathnameWithoutDefaultLang, curURL))
   }
 
   if (!hasLang) {
-    return NextResponse.rewrite(new URL(`/${defaultLang}${pathname}`, url))
+    return NextResponse.rewrite(
+      new URL(`/${defaultLang}${curPathname}`, curURL),
+    )
   }
 
   return NextResponse.next()
