7e5ec6a25df7b6f21289ad9fdffdf883ab684b11
devjiwonchoi <devjiwonchoi@gmail.com>
2023-12-01 05:55:30 +0900

    modify authOptions file name to kebab

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export * from './actions'
-export { authOptions } from './authOptions'
+export { authOptions } from './auth-options'
 export { contribs } from './contribs'
 export { notionClient, getPostsInfo } from './notion'
