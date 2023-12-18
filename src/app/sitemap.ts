53a4a78d2ffae4bc6e8195cfd02372d24b4cf4ba
Jiwon Choi <devjiwonchoi@gmail.com>
2023-12-18 19:22:15 +0900

    refactor: remove www from url (#85)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.jiwonchoi.dev',
+      url: 'https://jiwonchoi.dev',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://www.jiwonchoi.dev/blog',
+      url: 'https://jiwonchoi.dev/blog',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.jiwonchoi.dev/request',
+      url: 'https://jiwonchoi.dev/request',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
