911be2fa01cf67e99c57c18c367af4282e5491bf
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-03 02:06:01 +0900

    chore: remove current

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { AuthForm } from './AuthForm'
-export { ProfileCard } from './ProfileCard'
-export { RequestForm } from './RequestForm'
--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './actions'
-export { authOptions } from './auth-options'
-export { contribs } from './contribs'
