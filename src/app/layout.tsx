4f8d4f27124293bdec2aec9a3e29bc2890ba09cb
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-04 09:58:09 +0900

    chore: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,51 @@
-import type { Metadata } from 'next'
-import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { ProfileCard } from '@/components'
+import { GeistMono } from 'geist/font/mono'
 import { Header, Footer, NavBar } from '@/components/layouts'
 import '@/globals.css'
+import type { Metadata } from 'next'
 
-export default async function LangRootLayout({
+const description =
+  'Jiwon Choi is a software engineer concentrating on web, cloud, and developer experience. Active contributor of Next.js, Vercel, and other open-source projects.'
+
+const keywords = [
+  'Jiwon Choi',
+  'Vercel',
+  'Next.js',
+  'Software Engineer',
+  'Open Source',
+  'Web',
+  'Frontend',
+  'Cloud',
+  'Developer Experience',
+  'DevOps',
+]
+
+export const metadata: Metadata = {
+  title: 'Jiwon Choi',
+  description,
+  keywords,
+  metadataBase: new URL('https://www.jiwonchoi.dev'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.jiwonchoi.dev',
+    images: [
+      {
+        url: '/me-logo800x600.png',
+        width: 800,
+        height: 600,
+        alt: 'Jiwon Choi Logo',
+      },
+    ],
+    siteName: 'Jiwon Choi',
+  },
+  twitter: {
+    creator: '@devjiwonchoi',
+  },
+}
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
