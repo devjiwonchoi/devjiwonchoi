30436fbc2bce27423f8aea94a32dbde086aa2e97
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-06 06:14:38 +0900

    feat: terminal

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import { useEffect, useRef } from 'react'
 import { Terminal as XtermTerminal } from '@xterm/xterm'
 import { FitAddon } from '@xterm/addon-fit'
@@ -24,6 +25,7 @@ const XtermTerinalOptions: ITerminalOptions & ITerminalInitOnlyOptions = {
 }
 
 export function Terminal() {
+  const router = useRouter()
   const terminalRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
@@ -34,16 +36,48 @@ export function Terminal() {
     terminal.loadAddon(webLinksAddon)
     fitAddon.fit()
 
-    const promptStr = 'guest@jiwonchoi.dev ~ % '
+    let currentDir = '~'
+    const promptStr = (dir?: string) => `guest@jiwonchoi.dev ${dir ?? '~'} % `
     const promptStrLen = promptStr.length
 
-    const initPropmt = () => {
-      terminal.write(`\r${promptStr}`)
+    const homeDir = [
+      'biography',
+      'blog',
+      'projects',
+      // 'README.md',
+      // 'resume.pdf',
+      'socials',
+    ]
+    const innerDirs: Record<string, string[]> = {
+      biography: ['index.html'],
+      // TODO: fetch blog posts
+      blog: ['index.html'],
+      // TODO: fetch projects
+      projects: ['index.html'],
+      socials: ['github.md', 'gmail.md', 'linkedin.md', 'twitter.md'],
     }
-    const prompt = () => {
-      terminal.write(`\r\n${promptStr}`)
+    const helpMessage = [
+      '',
+      'Available commands:',
+      '  clear    - Clear the terminal screen',
+      '  help     - Show this help message',
+      '  ls       - List the files in the current directory',
+      '  whoami   - Print the current user',
+      '  cd       - Change the current directory',
+      '  open     - Open a file or directory',
+    ].join('\n\r')
+
+    const initPropmt = () => {
+      terminal.write(`\r${promptStr()}`)
+    }
+    const prompt = ({
+      dir = currentDir,
+      lineBreak = true,
+    }: { dir?: string; lineBreak?: boolean } = {}) => {
+      terminal.write(`\r${lineBreak ? '\n' : ''}${promptStr(dir)}`)
     }
     const commands: Record<string, any> = {
+      // TODO: fix it puts a new line after clear
       clear: {
         f: () => {
           terminal.reset()
@@ -52,20 +86,55 @@ export function Terminal() {
       },
       help: {
         f: () => {
-          terminal.writeln('\nAvailable commands:')
-          terminal.writeln('  clear - Clear the terminal screen')
-          terminal.writeln('  help - Show this help message')
+          terminal.writeln(helpMessage)
           prompt()
+        },
+      },
+      ls: {
+        f: () => {
+          // show the list of files in the current directory
+          if (currentDir in innerDirs) {
+            terminal.write(innerDirs[currentDir].join('    '))
+            prompt()
+          } else {
+            terminal.write(homeDir.join('    '))
+            prompt()
+          }
+        },
+      },
+      whoami: {
+        f: () => {
+          // TODO: cheer up the user
+          terminal.writeln('guest')
+          prompt()
+        },
+      },
+      cd: {
+        f: (dir: string) => {
+          // move to the given directory
+          if (dir in innerDirs) {
+            // innerDirs[dir]
+            currentDir = dir
+            prompt({ dir, lineBreak: false })
+          }
+        },
+      },
+      open: {
+        f: (file: string) => {
+          if (innerDirs[currentDir].includes(file)) {
+            router.push(`/${file === 'index.html' ? currentDir : file}`)
+          }
         },
       },
     }
 
     function runCommand(text: string) {
       const command = text.trim().split(' ')[0]
+      const dir = text.trim().split(' ').slice(1)[0]
       if (command.length > 0) {
         terminal.writeln('')
         if (command in commands) {
-          commands[command].f()
+          commands[command].f(dir)
           return
         }
         terminal.write(`zsh: command not found: ${command}`)
@@ -100,6 +169,31 @@ export function Terminal() {
             }
           }
           break
+        // Tab
+        case '\u0009':
+          // tab completion
+          const lastWord = command.split(' ').pop()
+
+          if (lastWord) {
+            let dir = homeDir
+            if (currentDir in innerDirs) {
+              dir = innerDirs[currentDir]
+            }
+
+            const matchingCommands = dir.filter((command) =>
+              command.startsWith(lastWord),
+            )
+
+            if (matchingCommands.length === 1) {
+              terminal.write(matchingCommands[0].slice(lastWord.length))
+              command += matchingCommands[0].slice(lastWord.length)
+            } else if (matchingCommands.length > 1) {
+              terminal.writeln('')
+              terminal.write(matchingCommands.join(' '))
+              prompt()
+            }
+          }
+          break
         // Left arrow
         case '\u001b[D':
           if (terminal.buffer.active.cursorX > promptStrLen) {
@@ -115,6 +209,9 @@ export function Terminal() {
         // Up arrow
         case '\u001b[A':
           if (historyIndex > 0) {
+            // clear the current command first
+            terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
+            // preserve current index, then decrement
             historyIndex--
             command = history[historyIndex]
             terminal.write(`\r${promptStr}${command}`)
@@ -123,6 +220,8 @@ export function Terminal() {
         // Down arrow
         case '\u001b[B':
           if (historyIndex < history.length - 1) {
+            // clear the current command first
+            terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
             historyIndex++
             command = history[historyIndex]
             terminal.write(`\r${promptStr}${command}`)
@@ -142,12 +241,13 @@ export function Terminal() {
 
     if (terminalRef.current) {
       terminal.open(terminalRef.current)
+      terminal.focus()
     }
 
     return () => {
       terminal.dispose()
     }
-  }, [])
+  }, [router])
 
   return (
     <div className="flex justify-center">
