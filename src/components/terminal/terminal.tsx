d4535fc8a5e19e473ebd19160e6bd8e63efd32e7
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-12 00:23:44 +0900

    refactor: split functions

--- src/components/terminal/terminal.tsx
+++ src/components/terminal/terminal.tsx
@@ -5,7 +5,6 @@ import { Terminal as XtermTerminal } from '@xterm/xterm'
 import { FitAddon } from '@xterm/addon-fit'
 import { WebLinksAddon } from '@xterm/addon-web-links'
 import '@xterm/xterm/css/xterm.css'
-import { dirTree } from './constants'
 import type { ITerminalInitOnlyOptions, ITerminalOptions } from '@xterm/xterm'
 
 const XtermTerminalOptions: ITerminalOptions & ITerminalInitOnlyOptions = {
@@ -25,6 +24,196 @@ const XtermTerminalOptions: ITerminalOptions & ITerminalInitOnlyOptions = {
   },
 }
 
+const promptStr = `guest@jiwonchoi.dev ~ % `
+
+const helpMessage = [
+  '',
+  'Available Commands:',
+  '  clear  -  Clear the terminal screen',
+  '  help   -  Show this help message',
+  '  ls     -  List the files in the current directory',
+  '  open   -  Open a file',
+].join('\n\r')
+
+const files = ['biography.php', 'blog.php', 'projects.php']
+
+function registerOnData({
+  terminal,
+  runCommand,
+  prompt,
+  history,
+  historyIndex,
+  command,
+}: {
+  terminal: XtermTerminal
+  runCommand: (text: string) => void
+  prompt: (lineBreak?: boolean) => void
+  history: string[]
+  historyIndex: number
+  command: string
+}) {
+  terminal.onData((e) => {
+    switch (e) {
+      case '\r': // Enter
+        runCommand(command)
+        history.push(command)
+        historyIndex = history.length
+        command = ''
+        break
+      case '\u007F': // Backspace (DEL)
+        // Do not delete the prompt
+        if (terminal.buffer.active.cursorX > promptStr.length) {
+          terminal.write('\b \b')
+          if (command.length > 0) {
+            command = command.substring(0, command.length - 1)
+          }
+        }
+        break
+      // Tab
+      case '\u0009':
+        // tab completion
+        const lastWord = command.split(' ').pop()
+
+        if (lastWord) {
+          const matchingCommands = files.filter((command) =>
+            command.startsWith(lastWord),
+          )
+
+          if (matchingCommands.length === 1) {
+            const completeCommand = matchingCommands[0].slice(lastWord.length)
+            terminal.write(completeCommand)
+            command += completeCommand
+          }
+
+          if (matchingCommands.length > 1) {
+            terminal.writeln('')
+            terminal.write(matchingCommands.join('    '))
+            prompt()
+            command = ''
+          }
+        }
+        break
+      // Left arrow
+      case '\u001b[D':
+        if (terminal.buffer.active.cursorX > promptStr.length) {
+          terminal.write('\u001b[D')
+        }
+        break
+      // Right arrow
+      case '\u001b[C':
+        if (
+          command.length >
+          terminal.buffer.active.cursorX - promptStr.length
+        ) {
+          terminal.write('\u001b[C')
+        }
+        break
+      // Up arrow
+      case '\u001b[A':
+        if (historyIndex > 0) {
+          // clear the current command first
+          terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
+          // preserve current index, then decrement
+          historyIndex--
+          command = history[historyIndex]
+          terminal.write(`\r${promptStr}${command}`)
+        }
+        break
+      // Down arrow
+      case '\u001b[B':
+        if (historyIndex < history.length - 1) {
+          // clear the current command first
+          terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
+          historyIndex++
+          command = history[historyIndex]
+          terminal.write(`\r${promptStr}${command}`)
+        }
+        break
+      default: // Print all other characters for demo
+        if (
+          (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7e)) ||
+          e >= '\u00a0'
+        ) {
+          command += e
+          terminal.write(e)
+        }
+    }
+  })
+}
+
+function runCommand({
+  terminal,
+  text,
+  prompt,
+  commands,
+}: {
+  terminal: XtermTerminal
+  text: string
+  prompt: () => void
+  commands: Record<string, any>
+}) {
+  const args = text.trim().split(' ')
+  const command = args[0]
+  const possiblyFile = args.slice(1)[0]
+
+  if (command.length) {
+    terminal.writeln('')
+
+    if (command in commands) {
+      return commands[command].f(possiblyFile)
+    }
+
+    terminal.write(`zsh: command not found: ${command}`)
+  }
+  prompt()
+}
+
+function getCommands({
+  terminal,
+  prompt,
+  router,
+}: {
+  terminal: XtermTerminal
+  prompt: (lineBreak?: boolean) => void
+  router: ReturnType<typeof useRouter>
+}) {
+  const commands: Record<string, any> = {
+    clear: {
+      f: () => {
+        // Reason why I don't use terminal.clear() or terminal.reset()
+        // See https://github.com/xtermjs/xterm.js/issues/950#issuecomment-327624355
+        terminal.write('\x1bc')
+        prompt(false)
+      },
+    },
+    help: {
+      f: () => {
+        terminal.writeln(helpMessage)
+        prompt()
+      },
+    },
+    ls: {
+      f: () => {
+        terminal.write(files.join('    '))
+        prompt()
+      },
+    },
+    open: {
+      f: (file: string) => {
+        if (files.includes(file)) {
+          const path = file.replace('.php', '')
+          return router.push(`/${path}`)
+        }
+
+        terminal.write(`The file "${file}" does not exist.`)
+        prompt()
+      },
+    },
+  }
+
+  return commands
+}
+
 export function Terminal() {
   const router = useRouter()
   const terminalRef = useRef<HTMLDivElement>(null)
@@ -37,208 +226,32 @@ export function Terminal() {
     terminal.loadAddon(webLinksAddon)
     fitAddon.fit()
 
-    let currentDir = '~'
-    const promptStr = (dir?: string) => `guest@jiwonchoi.dev ${dir ?? '~'} % `
-    const promptStrLen = promptStr.length
-
-    let currentDirLevel = 0
-    const helpMessage = [
-      '',
-      'Available commands:',
-      '  clear    - Clear the terminal screen',
-      '  help     - Show this help message',
-      '  ls       - List the files in the current directory',
-      '  whoami   - Print the current user',
-      '  cd       - Change the current directory',
-      '  open     - Open a file or directory',
-    ].join('\n\r')
-
-    const initPrompt = () => {
-      terminal.write(`\r${promptStr()}`)
-    }
-    const prompt = ({
-      dir = currentDir,
-      lineBreak = true,
-    }: { dir?: string; lineBreak?: boolean } = {}) => {
-      terminal.write(`\r${lineBreak ? '\n' : ''}${promptStr(dir)}`)
-    }
-    const commands: Record<string, any> = {
-      // TODO: fix it puts a new line after clear
-      clear: {
-        f: () => {
-          terminal.reset()
-          initPrompt()
-        },
-      },
-      help: {
-        f: () => {
-          terminal.writeln(helpMessage)
-          prompt()
-        },
-      },
-      ls: {
-        f: () => {
-          // show the list of files in the current directory
-          if (currentDir in dirTree) {
-            terminal.write(
-              dirTree[currentDir].files
-                .map((file: any) => file.name)
-                .join('    '),
-            )
-            prompt()
-          } else {
-            terminal.write(Object.keys(dirTree).join('    '))
-            prompt()
-          }
-        },
-      },
-      whoami: {
-        f: () => {
-          // TODO: cheer up the user
-          terminal.write('guest')
-          prompt()
-        },
-      },
-      cd: {
-        f: (dir: string) => {
-          // move to the given directory
-          if (dir in dirTree) {
-            currentDir = dir
-            prompt({ dir, lineBreak: false })
-          }
-          if (dir === '..') {
-            const dirArr = currentDir.split('/')
-            if (dirArr.length > 1) {
-              dirArr.pop()
-              currentDir = dirArr.join('/')
-              prompt({ dir: currentDir, lineBreak: false })
-            }
-          }
-        },
-      },
-      open: {
-        f: (file: string) => {
-          if (dirTree[currentDir].files.includes(file)) {
-            router.push(`/${file === 'index.html' ? currentDir : file}`)
-          }
-        },
-      },
-    }
-
-    function runCommand(text: string) {
-      const command = text.trim().split(' ')[0]
-      const dir = text.trim().split(' ').slice(1)[0]
-      if (command.length > 0) {
-        terminal.writeln('')
-        if (command in commands) {
-          commands[command].f(dir)
-          return
-        }
-        terminal.write(`zsh: command not found: ${command}`)
-      }
-      prompt()
+    const prompt = (lineBreak = true) => {
+      terminal.write(`\r${lineBreak ? '\n' : ''}${promptStr}`)
     }
 
     // command history
     const history: string[] = []
-    let historyIndex = 0
-
-    initPrompt()
-    let command = ''
-    terminal.onData((e) => {
-      switch (e) {
-        case '\u0003': // Ctrl+C
-          terminal.write('^C')
-          prompt()
-          break
-        case '\r': // Enter
-          runCommand(command)
-          history.push(command)
-          historyIndex = history.length
-          command = ''
-          break
-        case '\u007F': // Backspace (DEL)
-          // Do not delete the prompt
-          if (terminal.buffer.active.cursorX > promptStrLen) {
-            terminal.write('\b \b')
-            if (command.length > 0) {
-              command = command.substr(0, command.length - 1)
-            }
-          }
-          break
-        // Tab
-        case '\u0009':
-          // tab completion
-          const lastWord = command.split(' ').pop()
-
-          if (lastWord) {
-            let dir = Object.keys(dirTree)
-            if (currentDir in dirTree) {
-              dir = dirTree[currentDir]
-            }
-
-            const matchingCommands = dir.filter((command) =>
-              command.startsWith(lastWord),
-            )
-
-            if (matchingCommands.length === 1) {
-              terminal.write(matchingCommands[0].slice(lastWord.length))
-              command += matchingCommands[0].slice(lastWord.length)
-            } else if (matchingCommands.length > 1) {
-              terminal.writeln('')
-              terminal.write(matchingCommands.join(' '))
-              prompt()
-            }
-          }
-          break
-        // Left arrow
-        case '\u001b[D':
-          if (terminal.buffer.active.cursorX > promptStrLen) {
-            terminal.write('\u001b[D')
-          }
-          break
-        // Right arrow
-        case '\u001b[C':
-          if (command.length > terminal.buffer.active.cursorX - promptStrLen) {
-            terminal.write('\u001b[C')
-          }
-          break
-        // Up arrow
-        case '\u001b[A':
-          if (historyIndex > 0) {
-            // clear the current command first
-            terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
-            // preserve current index, then decrement
-            historyIndex--
-            command = history[historyIndex]
-            terminal.write(`\r${promptStr}${command}`)
-          }
-          break
-        // Down arrow
-        case '\u001b[B':
-          if (historyIndex < history.length - 1) {
-            // clear the current command first
-            terminal.write(`\r${promptStr}${' '.repeat(command.length)}`)
-            historyIndex++
-            command = history[historyIndex]
-            terminal.write(`\r${promptStr}${command}`)
-          }
-          break
-        default: // Print all other characters for demo
-          if (
-            (e >= String.fromCharCode(0x20) &&
-              e <= String.fromCharCode(0x7e)) ||
-            e >= '\u00a0'
-          ) {
-            command += e
-            terminal.write(e)
-          }
-      }
+    const commands = getCommands({
+      terminal,
+      prompt: (lineBreak?: boolean) => prompt(lineBreak),
+      router,
     })
+    let historyIndex = 0
+    let command = ''
 
     if (terminalRef.current) {
+      registerOnData({
+        terminal,
+        runCommand: (text) => runCommand({ terminal, text, prompt, commands }),
+        prompt,
+        history,
+        historyIndex,
+        command,
+      })
       terminal.open(terminalRef.current)
       terminal.focus()
+      prompt(false)
     }
 
     return () => {
