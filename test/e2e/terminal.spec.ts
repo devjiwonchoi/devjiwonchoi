43cecf8c209cb63e21646ad23962e6df363b5009
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-12 04:14:18 +0900

    test: refactor test cases

--- test/e2e/terminal.spec.ts
+++ test/e2e/terminal.spec.ts
@@ -1,82 +1,77 @@
 import { test, expect } from '@playwright/test'
+import type { Page } from '@playwright/test'
+
+const testCases = [
+  {
+    name: 'help - should display available commands',
+    command: 'help',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForSelector('.xterm-rows > div:nth-child(3) > span')
+      expect(
+        await page.textContent('.xterm-rows > div:nth-child(3) > span'),
+      ).toContain('Available Commands:')
+    },
+  },
+  {
+    name: 'ls - should display files',
+    command: 'ls',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForSelector('.xterm-rows > div:nth-child(2) > span')
+      expect(
+        await page.textContent('.xterm-rows > div:nth-child(2) > span'),
+      ).toContain('blog.php')
+    },
+  },
+  {
+    name: 'clear - should clear previous commands',
+    command: 'clear',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForSelector('.xterm-rows > div:nth-child(3)')
+      expect(
+        await page.textContent('.xterm-rows > div:nth-child(3)'),
+      ).not.toContain('blog.php')
+    },
+  },
+  {
+    name: 'open - should goto /biography',
+    command: 'open biography.php',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForURL('/biography')
+      expect(page.url()).toContain('/biography')
+    },
+  },
+  {
+    name: 'open - should goto /blog',
+    command: 'open blog.php',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForURL('/blog')
+      expect(page.url()).toContain('/blog')
+    },
+  },
+  {
+    name: 'open - should goto /projects',
+    command: 'open projects.php',
+    // some steps for expect
+    expected: async (page: Page) => {
+      await page.waitForURL('/projects')
+      expect(page.url()).toContain('/projects')
+    },
+  },
+]
 
 test.describe('terminal commands', () => {
-  test('help - should display available commands', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('help')
-    await page.keyboard.press('Enter')
-    await page.waitForSelector('.xterm-rows > div:nth-child(3) > span', {
-      state: 'attached',
+  for (const { name, command, expected } of testCases) {
+    test(name, async ({ page }) => {
+      await page.goto('/')
+      await page.waitForSelector('.xterm-rows')
+      await page.keyboard.type(command)
+      await page.keyboard.press('Enter')
+      await expected(page)
     })
-    expect(
-      await page.textContent('.xterm-rows > div:nth-child(3) > span'),
-    ).toContain('Available Commands:')
-  })
-
-  test('ls - should display files', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('ls')
-    await page.keyboard.press('Enter')
-    await page.waitForSelector('.xterm-rows > div:nth-child(2) > span', {
-      state: 'attached',
-    })
-    expect(
-      await page.textContent('.xterm-rows > div:nth-child(2) > span'),
-    ).toContain('blog.php')
-  })
-
-  test('clear - should clear previous commands', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('help')
-    await page.keyboard.press('Enter')
-    await page.waitForSelector('.xterm-rows > div:nth-child(3) > span', {
-      state: 'attached',
-    })
-    expect(
-      await page.textContent('.xterm-rows > div:nth-child(3) > span'),
-    ).toContain('Available Commands:')
-
-    // Clear previous help output
-    await page.keyboard.type('clear')
-    await page.keyboard.press('Enter')
-    await page.waitForSelector('.xterm-rows > div:nth-child(3)', {
-      state: 'attached',
-    })
-    expect(
-      await page.textContent('.xterm-rows > div:nth-child(3)'),
-    ).not.toContain('blog.php')
-  })
-
-  test('open - should goto /biography', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('open biography.php')
-    await page.keyboard.press('Enter')
-
-    await page.waitForURL('/biography')
-    expect(page.url()).toContain('/biography')
-  })
-
-  test('open - should goto /blog', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('open blog.php')
-    await page.keyboard.press('Enter')
-
-    await page.waitForURL('/blog')
-    expect(page.url()).toContain('/blog')
-  })
-
-  test('open - should goto /projects', async ({ page }) => {
-    await page.goto('/')
-    await page.waitForSelector('.xterm-rows', { state: 'attached' })
-    await page.keyboard.type('open projects.php')
-    await page.keyboard.press('Enter')
-
-    await page.waitForURL('/projects')
-    expect(page.url()).toContain('/projects')
-  })
+  }
 })
