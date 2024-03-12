11bc506fc7202e808b874da891d1d8a78eadd14d
Jiwon Choi <devjiwonchoi@gmail.com>
2024-03-13 06:57:37 +0900

    refactor: code split and lint (#127)
    * refactor: componentize terminal loading component
    
    * chore: run lint-fix
    
    * refactor: useMediaQuery

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect'
 
-/**
- * Custom hook that tells you whether a given media query is active.
- *
- * Inspired by https://usehooks.com/useMedia/
- * https://gist.github.com/gragland/ed8cac563f5df71d78f4a1fefa8c5633
- *
- * ref: https://dev.to/justincy/4-patterns-for-responsive-props-in-react-39ak#responsive-props
- */
-// TODO: replace with applicable better approach
-export default function useMediaQuery(query: string) {
-  const [matches, setMatches] = useState(false)
-  useEffect(
-    () => {
-      const mediaQuery = window.matchMedia(query)
-      setMatches(mediaQuery.matches)
-      const handler = (event: {
-        matches: boolean | ((prevState: boolean) => boolean)
-      }) => setMatches(event.matches)
-      mediaQuery.addEventListener('change', handler)
-      return () => mediaQuery.removeEventListener('change', handler)
-    },
-    [], // Empty array ensures effect is only run on mount and unmount
-  )
+// inspired by https://github.com/chakra-ui/chakra-ui/issues/3580#issuecomment-1131211586
+export function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState<boolean | null>(null)
+
+  useIsomorphicLayoutEffect(() => {
+    const queryList = window.matchMedia(query)
+    const listener = () => setMatches(queryList.matches)
+    listener()
+    queryList.addEventListener('change', listener)
+    return () => queryList.removeEventListener('change', listener)
+  }, [query])
+
   return matches
 }
