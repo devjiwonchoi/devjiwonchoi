89bb24a3943ea00a2c7615a4ac16c41fa7fa3363
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-13 08:22:47 +0900

    feat: add lighthouse ci

---
+++ .lighthouserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'pnpm build && pnpm start',
+      url: [process.env.BASE_URL ?? 'http://localhost:3000'],
+      numberOfRuns: 1,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
+    },
+  },
+}
