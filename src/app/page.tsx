aa2dd614d9dcdc06a243358b443d53ccd76b0581
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-13 01:53:11 +0900

    style: add loading state for terminal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,23 @@ const Terminal = dynamic(
   async () => (await import('@/components/terminal')).Terminal,
   {
     ssr: false,
+    loading: () => (
+      <div className="hidden h-[496px] rounded bg-neutral-900 p-2 md:block">
+        <div
+          style={{
+            width: '674px',
+            height: '24px',
+            lineHeight: '24px',
+            overflow: 'hidden',
+            color: '#fafafa',
+            fontSize: '14px',
+            fontFamily: 'Geist Mono, monospace',
+          }}
+        >
+          <span>Initiating Terminal...</span>
+        </div>
+      </div>
+    ),
   },
 )
 
