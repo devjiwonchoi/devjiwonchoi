a9856bfadfe781a5374ab5e763b7cd793f8ace1f
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-13 23:21:47 +0900

    test: add logo test

---
+++ test/e2e/logos/me-logo.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('logos - me-logo', () => {
+  test('should render me-logo at /', async ({ page }) => {
+    await page.goto('/')
+
+    const image = await page.$('img[alt="Jiwon Choi Logo"]')
+    expect(image).toBeTruthy()
+  })
+
+  test('should render me-logo at /biography', async ({ page }) => {
+    await page.goto('/biography')
+
+    const image = await page.$('img[alt="Jiwon Choi Logo"]')
+    expect(image).toBeTruthy()
+  })
+
+  test('should render me-logo at /projects', async ({ page }) => {
+    await page.goto('/projects')
+
+    const image = await page.$('img[alt="Jiwon Choi Logo"]')
+    expect(image).toBeTruthy()
+  })
+
+  test('should render me-logo at /blog', async ({ page }) => {
+    await page.goto('/blog')
+
+    const image = await page.$('img[alt="Jiwon Choi Logo"]')
+    expect(image).toBeTruthy()
+  })
+})
