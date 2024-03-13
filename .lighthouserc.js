8746e3f7572742f9105a747fa419b6d24fe83f7a
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-13 13:17:21 +0900

    chore: higher expectation on scores

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,10 +7,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.95 }],
-        'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:best-practices': ['error', { minScore: 0.95 }],
-        'categories:seo': ['error', { minScore: 0.95 }],
+        'categories:performance': ['error', { minScore: 0.97 }],
+        'categories:accessibility': ['error', { minScore: 0.97 }],
+        'categories:best-practices': ['error', { minScore: 0.97 }],
+        'categories:seo': ['error', { minScore: 0.97 }],
       },
     },
     upload: {
