66d6556186f5b7abecd6a12a6a456835847cba7e
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-15 22:46:24 +0900

    refactor: ensure filename to be consistent

--- scripts/setup-blog-posts.ts
+++ scripts/setup-blog-posts.ts
@@ -1,4 +1,4 @@
-import { mkdir, readdir, readFile, writeFile } from 'fs/promises'
+import { mkdir, readdir, readFile, rm, writeFile } from 'fs/promises'
 import { extname, join } from 'path'
 import matter from 'gray-matter'
 import type { BlogPost } from '@/utils/types'
@@ -6,34 +6,65 @@ import type { BlogPost } from '@/utils/types'
 const blogDocsDir = join(process.cwd(), 'docs', 'blog')
 const outputDir = join(process.cwd(), '.vercel', 'output')
 
-;(async function setUpBlogPosts() {
+// ref: https://github.com/leerob/leerob.io
+function slugify(str: string) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+}
+
+export async function setUpBlogPosts() {
+  // Read docs/blog directory
   const dirents = await readdir(blogDocsDir, { withFileTypes: true })
+  // Create .vercel/output directory to ensure it exists
   await mkdir(outputDir, { recursive: true })
 
   const postJobs = dirents.map(async (dirent) => {
+    // We are looking for a .mdx file
     if (!dirent.isFile()) return
-
     const direntName = dirent.name
     const ext = extname(direntName)
     if (ext !== '.mdx') return
 
-    const source = await readFile(join(dirent.path, direntName), 'utf-8')
+    // Read the file and parse as frontmatter and content
+    const resolvedDirentPath = join(dirent.path, direntName)
+    const source = await readFile(resolvedDirentPath, 'utf-8')
     const { data: frontmatter, content } = matter(source)
 
-    const slug = direntName.replace(ext, '')
-    const id = slug.split('-').pop()
+    // Generate slug and id here
+    // Ensure id is number-formatted string
+    const id: string = frontmatter.id.toString()
+    if (!id || isNaN(parseInt(id))) return
+    const slug = `${slugify(frontmatter.title)}-${id}`
+
+    // frontmatter parses date as a Date object
     // See https://github.com/jonschlinkert/gray-matter/issues/62
     const date = new Date(frontmatter.date)?.toISOString()?.split('T')?.[0]
 
-    if (!id || isNaN(parseInt(id))) return
+    // Write the post to .vercel/output as post-${id}.json
+    const post = JSON.stringify({ ...frontmatter, id, slug, content, date })
+    await writeFile(`${outputDir}/post-${id}.json`, post)
 
-    const json = JSON.stringify({ ...frontmatter, id, slug, content, date })
-    await writeFile(`${outputDir}/post-${id}.json`, json)
+    // Ensure the file names are consistent
+    const expectedDirentName = `${id}-${slug}${ext}`
+    if (direntName !== expectedDirentName) {
+      await rm(resolvedDirentPath)
+      await writeFile(join(dirent.path, expectedDirentName), source)
+    }
 
     return { ...frontmatter, id, slug, date } as BlogPost
   })
 
-  const posts = (await Promise.all(postJobs)).filter(Boolean) as BlogPost[]
-  const json = JSON.stringify(posts)
-  await writeFile(`${outputDir}/posts.json`, json)
-})()
+  // All those posts to a single posts.json file without content
+  const posts = JSON.stringify(
+    (await Promise.all(postJobs)).filter(Boolean) as BlogPost[],
+  )
+  await writeFile(`${outputDir}/posts.json`, posts)
+}
+
+setUpBlogPosts()
