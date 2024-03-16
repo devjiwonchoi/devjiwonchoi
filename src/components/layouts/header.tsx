2298e3a2993f47e3f0d067ad2273643a7b26a100
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-17 03:09:22 +0900

    refactor: svgs are optimized also

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -4,11 +4,12 @@ export function Header() {
   return (
     <header className="flex flex-col items-center pt-12 md:pt-20">
       <Image
+        src="/images/me-logo.svg"
         alt="Jiwon Choi Logo"
         className="h-20 w-20"
-        height={0}
-        src="/images/me-logo.svg"
-        width={0}
+        height={80}
+        width={80}
+        priority
       />
       <h1 className="mb-4 text-4xl font-bold text-neutral-50 sm:text-4xl">
         Jiwon Choi
