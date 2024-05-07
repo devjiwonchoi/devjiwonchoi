6fc67654f924b0d96ffb7fd645ed4b5b9995de03
Jiwon Choi <devjiwonchoi@gmail.com>
2024-05-08 01:56:13 +0900

    chore: use typedRoutes (#218)
    * chore: use typedRoutes
    
    * fix: type as Route

--- src/components/layouts/nav-bar.tsx
+++ src/components/layouts/nav-bar.tsx
@@ -1,3 +1,4 @@
+import type { Route } from 'next'
 import Link from 'next/link'
 
 const navItems = [
@@ -13,8 +14,8 @@ export function NavBar() {
         {navItems.map(({ path, name }) => (
           <li key={path}>
             <Link
-              className="relative m-1 flex py-1 px-2 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
-              href={path}
+              className="relative m-1 flex px-2 py-1 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
+              href={path as Route}
             >
               {name}
             </Link>
