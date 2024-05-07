31ee5f272bb320878b084e9f8ac013b541c2f674
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-07 23:42:35 +0900

    move

--- src/components/layouts/nav-bar.tsx
+++ src/components/layouts/nav-bar.tsx
@@ -1,36 +1,22 @@
-'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import clsx from 'clsx'
+
+const navItems = [
+  { path: '/', name: '::1' },
+  { path: '/blog', name: 'blog' },
+  { path: '/lab', name: 'lab' },
+]
 
 export function NavBar() {
-  const pathname = usePathname()
   return (
     <nav className="p-6">
       <ul className="flex justify-center space-x-6">
-        {[
-          ['::1', '/'],
-          // ['bio', '/bio'],
-          ['proj', '/projects'],
-          ['blog', '/blog'],
-        ].map(([title, url]) => (
-          <li key={title}>
+        {navItems.map(({ path, name }) => (
+          <li key={path}>
             <Link
-              className={clsx(
-                // by default, highlight and underline on hover
-                'text-lg hover:text-neutral-50 hover:underline md:text-base',
-                {
-                  // for `::1`, tight font spacing
-                  'tracking-tighter': url === '/',
-                  // highlight and underline the current page
-                  'text-neutral-200': pathname !== url,
-                  'text-neutral-50': pathname === url,
-                  underline: pathname === url,
-                },
-              )}
-              href={url}
+              className="relative m-1 flex py-1 px-2 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
+              href={path}
             >
-              {title}
+              {name}
             </Link>
           </li>
         ))}
