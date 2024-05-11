c0fb494f40cd5cd21c1441abe06910cbeeacaab1
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-11 21:44:35 +0900

    chore: apply verbatimModuleSyntax

--- src/utils/auth-options.ts
+++ src/utils/auth-options.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
