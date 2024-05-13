e99c007d9390cd98522049c1e56020b4064ce233
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-13 18:32:39 +0900

    chore: set metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,22 @@
+import type { Metadata } from 'next'
 import { BlogPosts } from '@/components/mdx/posts'
 
-export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+const description =
+  "Jiwon Choi's curation of personal thoughts, troubleshoots, and solutions to problems."
+
+const keywords = [
+  'jiwon choi',
+  'software engineer',
+  'tech blogs',
+  'dev blogs',
+  'developer blogs',
+  'software engineer blogs',
+]
+
+export const metadata: Metadata = {
+  title: "Jiwon Choi's Blog",
+  description,
+  keywords,
 }
 
 export default function Page() {
