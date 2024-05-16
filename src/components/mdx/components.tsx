e9e491465d6926ab96910934331d84033dd2374f
Jiwon Choi <devjiwonchoi@gmail.com>
2024-05-17 00:48:52 +0900

    chore: setup husky lint-staged (#259)
    * chore: setup husky
    
    * chore: configure lint staged
    
    * test
    
    * chore: add lint-staged
    
    * test
    
    * test
    
    * chore: husky v9
    
    * chore: pkg
    
    * chore: run lint-fix
    
    * remove test f u

--- src/components/mdx/components.tsx
+++ src/components/mdx/components.tsx
@@ -53,7 +53,7 @@ function RoundedImage(props: { alt: string; src: string }) {
 
 function Callout(props: { emoji: string; children: React.ReactNode }) {
   return (
-    <div className="mb-8 flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 py-3 px-4 text-sm text-neutral-900 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
+    <div className="mb-8 flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 px-4 py-3 text-sm text-neutral-900 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100">
       <div className="mr-4 flex w-4 items-center">{props.emoji}</div>
       <div className="callout w-full">{props.children}</div>
     </div>
