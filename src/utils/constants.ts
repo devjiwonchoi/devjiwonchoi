06ec62e6d923c72557e6c6684d740c97d340f6e3
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-09 17:11:44 +0900

    chore: no www

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_PROD = process.env.NODE_ENV === 'production'
-export const PROD_BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'www.jiwonchoi.dev'}`
+export const PROD_BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'jiwonchoi.dev'}`
