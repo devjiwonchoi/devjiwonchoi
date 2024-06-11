3e85ff153fabb6e16184d0b95b50163169ff4386
Jiwon Choi <devjiwonchoi@gmail.com>
2024-06-12 06:07:34 +0900

    fix: auth to page (#315)

--- src/app/nextjs/page.tsx
+++ src/app/nextjs/page.tsx
@@ -1,56 +1,13 @@
-'use client'
-
-import type { CoreMessage } from 'ai'
-import { useState } from 'react'
-import { readStreamableValue } from 'ai/rsc'
-import { continueConversation } from './actions'
+import { SignIn } from '@/components/sign-in'
+import { auth } from '@/utils/auth'
+import { Chat } from './chat'
 
 // Force the page to be dynamic and allow streaming responses up to 30 seconds
 export const dynamic = 'force-dynamic'
 export const maxDuration = 30
 
-export default function Chat() {
-  const [messages, setMessages] = useState<CoreMessage[]>([])
-  const [input, setInput] = useState('')
-  return (
-    <div className="stretch mx-auto mb-auto flex w-full max-w-md flex-col py-24">
-      {messages.map((m, i) => (
-        <div key={i} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content as string}
-        </div>
-      ))}
-
-      <form
-        action={async () => {
-          const newMessages: CoreMessage[] = [
-            ...messages,
-            { content: input, role: 'user' },
-          ]
-
-          setMessages(newMessages)
-          setInput('')
-
-          const result = await continueConversation(newMessages)
-
-          for await (const content of readStreamableValue(result)) {
-            setMessages([
-              ...newMessages,
-              {
-                role: 'assistant',
-                content: content as string,
-              },
-            ])
-          }
-        }}
-      >
-        <input
-          className="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </form>
-    </div>
-  )
+export default async function Nextjs() {
+  const session = await auth()
+  if (!session || (session as any).message) return <SignIn />
+  return <Chat />
 }
