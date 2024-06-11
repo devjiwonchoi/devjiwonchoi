c67a137e51637db2c47ebafd242a5f74dc448201
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-11 20:21:05 +0900

    style: update to rsc

--- src/app/nextjs/actions.ts
+++ src/app/nextjs/actions.ts
@@ -1,8 +1,8 @@
 'use server'
-
-import { createStreamableValue } from 'ai/rsc'
-import { type CoreMessage, streamText } from 'ai'
+import type { CoreMessage } from 'ai'
 import { openai } from '@ai-sdk/openai'
+import { streamText } from 'ai'
+import { createStreamableValue } from 'ai/rsc'
 
 export async function continueConversation(messages: CoreMessage[]) {
   const result = await streamText({
--- src/app/nextjs/page.tsx
+++ src/app/nextjs/page.tsx
@@ -1,26 +1,54 @@
 'use client'
 
-import { useChat } from '@ai-sdk/react'
+import type { CoreMessage } from 'ai'
+import { useState } from 'react'
+import { readStreamableValue } from 'ai/rsc'
+import { continueConversation } from './actions'
+
+// Force the page to be dynamic and allow streaming responses up to 30 seconds
+export const dynamic = 'force-dynamic'
+export const maxDuration = 30
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: 'api/nextjs',
-  })
+  const [messages, setMessages] = useState<CoreMessage[]>([])
+  const [input, setInput] = useState('')
   return (
     <div className="stretch mx-auto mb-auto flex w-full max-w-md flex-col py-24">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
+      {messages.map((m, i) => (
+        <div key={i} className="whitespace-pre-wrap">
           {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+          {m.content as string}
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      <form
+        action={async () => {
+          const newMessages: CoreMessage[] = [
+            ...messages,
+            { content: input, role: 'user' },
+          ]
+
+          setMessages(newMessages)
+          setInput('')
+
+          const result = await continueConversation(newMessages)
+
+          for await (const content of readStreamableValue(result)) {
+            setMessages([
+              ...newMessages,
+              {
+                role: 'assistant',
+                content: content as string,
+              },
+            ])
+          }
+        }}
+      >
         <input
           className="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
           value={input}
           placeholder="Say something..."
-          onChange={handleInputChange}
+          onChange={(e) => setInput(e.target.value)}
         />
       </form>
     </div>
