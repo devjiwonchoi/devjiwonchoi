3cea6b2a4bfb02bb67c8dbc74015a9b835ce2b44
Jiwon Choi <devjiwonchoi@gmail.com>
2024-06-26 01:03:47 +0900

    update (#342)

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,4 @@
+export default {
+  '*.{js,mjs,jsx,ts,tsx}': ['prettier --write'],
+  '*.{css,json,md,mdx,yml,yaml}': ['prettier --write'],
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/{app,components}/**/*.{ts,tsx}'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
