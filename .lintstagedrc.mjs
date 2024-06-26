5bec5bdbacf099d209150316ca0277eac1888799
Jiwon Choi <devjiwonchoi@gmail.com>
2024-06-27 03:10:59 +0900

    refactor project (#346)
    * wip
    
    * restore postcss

--- .lintstagedrc.mjs
+++
@@ -1,4 +0,0 @@
-export default {
-  '*.{js,mjs,jsx,ts,tsx}': ['prettier --write'],
-  '*.{css,json,md,mdx,yml,yaml}': ['prettier --write'],
-}
---
+++ postcss.config.mjs
@@ -0,0 +1,6 @@
+// TODO: remove this when https://github.com/vercel/next.js/issues/67218 is fixed
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
