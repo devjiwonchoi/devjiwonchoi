1a2b2d020a1d4a600175e585633088042c60aac6
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-27 02:37:51 +0900

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     ppr: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/robots',
+        destination: '/robots.txt',
+        permanent: true,
+      },
+      {
+        source: '/sitemap',
+        destination: '/sitemap.xml',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
