c985f3fbb7e15ee19aa2346084d9eb51222516aa
Jiwon Choi <devjiwonchoi@gmail.com>
2024-07-03 20:23:28 +0900

    perf: early return (#355)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
 import type { NextRequest } from 'next/server'
-import { get, has } from '@vercel/edge-config'
+import { get } from '@vercel/edge-config'
 import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  // if the path is /, zero computation
   if (request.nextUrl.pathname === '/') {
-    if (request.nextUrl.hostname === 'jiw.one') {
-      return NextResponse.redirect('https://jiwonchoi.dev', 301)
-    }
-
     return NextResponse.next()
   }
 
   const paths = request.nextUrl.pathname.split('/')
-  const possibleKey = paths[1]!
+  // if the path is not /<key>, we don't need to check the Edge Config
+  if (paths.length !== 2) {
+    return NextResponse.next()
+  }
 
-  if (await has(possibleKey)) {
-    return NextResponse.redirect((await get(possibleKey)) as string)
+  // ['', '<key>']
+  const key = await get(paths[1])
+  if (typeof key === 'string') {
+    return NextResponse.redirect(key)
   }
 
   return NextResponse.next()
