de4ac47d6e72865a036972fbdfb58341b11c4af4
devjiwonchoi <devjiwonchoi@gmail.com>
2024-07-04 23:36:01 +0900

    chore: do not email on unauth req

--- src/app/api/streak-freezer/route.ts
+++ src/app/api/streak-freezer/route.ts
@@ -6,33 +6,19 @@ import { sendEmail } from './send-email'
 export async function GET(request: NextRequest) {
   // See https://vercel.com/docs/cron-jobs/manage-cron-jobs#securing-cron-jobs
   const authHeader = request.headers.get('authorization')
-  if (authHeader === `Bearer ${process.env.CRON_SECRET}`) {
-    const username = 'devjiwonchoi'
-
-    if (await hasShipped(username)) {
-      await sendEmail({ subject: 'Has Shipped' })
-      return new Response('Already shipped today.', { status: 200 })
-    }
-
-    const response = await commit(username)
-    await sendEmail({ subject: response })
-
-    return new Response(response, { status: 201 })
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized.', { status: 401 })
   }
 
-  const unauthorizedRequest = {
-    ip: request.ip,
-    country: request.geo?.country,
-    city: request.geo?.city,
-    region: request.geo?.region,
-    latitude: request.geo?.latitude,
-    longitude: request.geo?.longitude,
-    userAgent: request.headers.get('user-agent'),
+  const username = 'devjiwonchoi'
+
+  if (await hasShipped(username)) {
+    await sendEmail({ subject: 'Has Shipped' })
+    return new Response('Already shipped today.', { status: 200 })
   }
 
-  await sendEmail({
-    subject: 'Unauthorized',
-    text: `Unauthorized Request: ${JSON.stringify(unauthorizedRequest, null, 2)}`,
-  })
-  return new Response('Unauthorized.', { status: 401 })
+  const response = await commit(username)
+  await sendEmail({ subject: response })
+
+  return new Response(response, { status: 201 })
 }
