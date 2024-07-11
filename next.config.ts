cd1c24869e765fb760331ee16fcf2ea9c08f6212
devjiwonchoi <devjiwonchoi@gmail.com>
2024-07-11 15:40:48 +0900

    chore: enable typedEnv

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/robots',
-        destination: '/robots.txt',
-        permanent: true,
-      },
-      {
-        source: '/sitemap',
-        destination: '/sitemap.xml',
-        permanent: true,
-      },
-    ]
+    typedEnv: true,
   },
 }
 
