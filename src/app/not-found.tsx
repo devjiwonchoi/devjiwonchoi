2c535d8d943cfef5370e74552d11aed00616f12d
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-16 17:15:23 +0900

    build success

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function NotFound() {
-  return (
-    <Link href="/" className="underline">
-      404 Not Found: Return Home
-    </Link>
-  )
-}
--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const IS_DEV = process.env.NODE_ENV === 'development'
-export const IS_PROD = process.env.NODE_ENV === 'production'
-export const PROD_BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'jiwonchoi.dev'}`
