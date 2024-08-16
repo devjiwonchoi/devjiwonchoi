75b6cfaae300408de9d4100758e793da7bcf504a
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-16 20:30:23 +0900

    has-shipped

--- apps/jiwone/middleware.ts
+++ apps/jiwone/middleware.ts
@@ -2,20 +2,22 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { get } from '@vercel/edge-config'
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect('https://jiwonchoi.dev', { status: 301 })
-  }
+  if (request.headers.has('x-vercel-signature')) {
+    if (request.nextUrl.pathname === '/') {
+      return NextResponse.redirect('https://jiwonchoi.dev', { status: 301 })
+    }
 
-  const paths = request.nextUrl.pathname.split('/')
-  // if the path is not /<key>, we don't need to check the Edge Config
-  if (paths.length !== 2) {
-    return NextResponse.next()
-  }
+    const paths = request.nextUrl.pathname.split('/')
+    // if the path is not /<key>, we don't need to check the Edge Config
+    if (paths.length !== 2) {
+      return NextResponse.next()
+    }
 
-  // ['', '<key>']
-  const key = await get(paths[1])
-  if (typeof key === 'string') {
-    return NextResponse.redirect(key, { status: 307 })
+    // ['', '<key>']
+    const key = await get(paths[1])
+    if (typeof key === 'string') {
+      return NextResponse.redirect(key, { status: 307 })
+    }
   }
 
   return NextResponse.next()
