3c32a0c0eb3559502eba7be1d177cb6cbfbcba60
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-21 05:28:25 +0900

    wip

--- apps/luv/app/layout.tsx
+++ apps/luv/app/layout.tsx
@@ -1,13 +1,35 @@
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+const fontHeading = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn('antialiased', fontHeading.variable, fontBody.variable)}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
