dce6265ee4df3fa5537d7d9193af7f0c9c05349b
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-15 20:27:34 +0200

    biome tailwind

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,6 +2,7 @@ const lintStagedConfig = {
   '*.{js,mjs,jsx,ts,tsx}': [
     'next lint --max-warnings=0 --fix --file',
     'biome format --write --no-errors-on-unmatched',
+    'biome lint --write --unsafe',
   ],
   '*.{css,json,md,mdx,yml,yaml}': [
     'biome format --write --no-errors-on-unmatched',
