0424634f7f3714d65cad30dfb509b73269d92b79
Jiwon Choi <devjiwonchoi@gmail.com>
2025-05-15 19:33:45 +0200

    dio (#657)

---
+++ src/app/learnings/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+import { readdir } from 'fs/promises'
+import { Suspense } from 'react'
+import {
+  unstable_cacheLife as cacheLife,
+  unstable_cacheTag as cacheTag,
+} from 'next/cache'
+
+async function getPostSlugs() {
+  'use cache'
+  const dir = 'public/md/learnings'
+  const postSlugs = (await readdir(dir)).map((file) => file.replace('.md', ''))
+
+  cacheTag('learnings')
+  cacheLife('hours')
+
+  return postSlugs
+}
+
+export default async function Learnings() {
+  const postSlugs = await getPostSlugs()
+  return (
+    <Suspense fallback={<div>Loading Learnings...</div>}>
+      {postSlugs.map((slug) => (
+        <div key={slug}>
+          <Link href={`/learnings/${slug}`}>{slug}</Link>
+        </div>
+      ))}
+    </Suspense>
+  )
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const PROD_BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'devjiwonchoi.com'}`
+export const PROD_BASE_URL = 'https://devjiwonchoi.com'
