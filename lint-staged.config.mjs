342633a36ffdeaca83c7c83389c648021b156a77
Jiwon Choi <devjiwonchoi@gmail.com>
2025-05-15 22:54:26 +0200

    remove biome, back prettier (#665)
    * remove biome, back prettier
    
    * run prettier

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,9 @@
 const lintStagedConfig = {
   "*.{js,mjs,jsx,ts,tsx}": [
     "next lint --max-warnings=0 --fix --file",
-    "biome format --write --no-errors-on-unmatched",
-    "biome lint --write --unsafe",
+    "prettier --write",
   ],
-  "*.{css,json,md,mdx,yml,yaml}": [
-    "biome format --write --no-errors-on-unmatched",
-  ],
+  "*.{css,json,md,mdx,yml,yaml}": ["prettier --write"],
 };
 
 export default lintStagedConfig;
--- public/md/learnings/test.md
+++ public/md/learnings/test.md
@@ -1,9 +1,9 @@
 ---
-title: 'Test'
-description: 'This is a test learning page.'
-keywords: ['test', 'learning', 'page']
+title: "Test"
+description: "This is a test learning page."
+keywords: ["test", "learning", "page"]
 draft: true
-createdAt: 'April 7, 2025'
+createdAt: "April 7, 2025"
 ---
 
 This is a test learning page.
