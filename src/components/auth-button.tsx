98730ea2a9642670887460bfa48344988824e912
devjiwonchoi <devjiwonchoi@gmail.com>
2025-06-04 03:26:37 +0200

    fix

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -2,13 +2,12 @@
 
 import { signIn, signOut } from "next-auth/react";
 import { LogIn, LogOut } from "lucide-react";
-import { Button } from "./ui/button";
 
 export function SignIn() {
   return (
     <div
       className="flex cursor-pointer items-center gap-2 rounded-md p-2 hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-100"
-      onClick={() => signIn()}
+      onClick={() => signIn("github")}
     >
       <LogIn className="mr-2 h-4 w-4" />
       Sign in
@@ -18,9 +17,12 @@ export function SignIn() {
 
 export function SignOut() {
   return (
-    <Button onClick={() => signOut()}>
+    <div
+      className="flex cursor-pointer items-center gap-2 rounded-md p-2 hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-100"
+      onClick={() => signOut()}
+    >
       <LogOut className="mr-2 h-4 w-4" />
       Sign out
-    </Button>
+    </div>
   );
 }
