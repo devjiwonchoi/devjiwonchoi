77d88333e50aa8ff691289966fe5cf2ad5a15c7e
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-10 17:40:38 +0200

    nits

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     cacheComponents: true,
-    ppr: true,
     typedEnv: true,
     typedRoutes: true,
     turbopackPersistentCaching: true,
-    devtoolSegmentExplorer: true,
     clientSegmentCache: true,
   },
 };
