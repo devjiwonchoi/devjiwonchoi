538581369a39c1157ac33fd715bb9dc8875e4a9e
Jiwon Choi <devjiwonchoi@gmail.com>
2025-10-21 05:55:43 +0200

    Update stuff (#790)
    * Update stuff
    - eslint flat config
    - next command remove --turbopack
    - move next options out of experimental
    
    * frozen lockfile

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
   { ignores: [".next/**", ".vercel/**", ".turbo/**"] },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
 ];
 
 export default eslintConfig;
